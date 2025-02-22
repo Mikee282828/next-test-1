9e43eb3f1900496e01e23e90e7c4e7e6815dde46
MikeLaptop <micheledong2803@gmail.com>
2025-02-22 21:27:49 +0100

    font importation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { lusitana } from "./fonts";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
-    </div>
+    <>
+      <div className={`text-3xl ${lusitana.className}`}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi vero
+        quia quos non pariatur qui, dignissimos reiciendis voluptatibus amet cum
+        quasi ad sapiente omnis? Beatae corrupti quidem nam voluptatum
+        recusandae.
+        <br />
+        {lusitana.className}
+      </div>
+    </>
   );
 }
